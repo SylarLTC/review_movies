5d5f0e33fb87c2d41b8efd3affd805e30fee5236
Adilzhan <abalgabekov@gmail.com>
2023-05-24 12:54:17 +0100

    Logging Users In

--- backend/models/userModel.ts
+++ backend/models/userModel.ts
@@ -43,4 +43,26 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+// static login method
+userSchema.statics.login = async function (email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw Error("Invalid login credentials");
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw Error("Invalid login credentials");
+  }
+
+  return user;
+};
+
 module.exports = mongoose.model("User", userSchema);
