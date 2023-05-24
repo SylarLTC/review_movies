cf91bf7733f99518e2b2362559b4d83894bd428c
Adilzhan <abalgabekov@gmail.com>
2023-05-24 13:16:05 +0100

    React Auth Context

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { MoviesContextProvider } from "./context/MovieContext";
 import { MoviesAPIContextProvider } from "./context/MovieAPIContext";
+import { AuthContextProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <AuthContextProvider>
     <MoviesContextProvider>
       <MoviesAPIContextProvider>
         <App />
       </MoviesAPIContextProvider>
     </MoviesContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
