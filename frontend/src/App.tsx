1ac9bd319c287b4a4bfd3844caeb6243718ad01c
Adilzhan <abalgabekov@gmail.com>
2023-05-30 15:49:16 +0100

    Protecting React Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,30 @@
 import { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import { useMoviesContext } from "./hooks/useMoviesContext";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+// pages and components
 import { Header } from "./components/Header";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { MovieForm } from "./pages/MovieForm";
-import { useMoviesContext } from "./hooks/useMoviesContext";
 
 export const App = () => {
   const [title, setTitle] = useState<string>("");
   const { movies } = useMoviesContext();
+  const {user} = useAuthContext();
+
   return (
     <div className="App">
       <BrowserRouter>
         <Header title={title} setTitle={setTitle} movies={movies} />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home title={title} />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+            <Route path="/" element={user ? <Home title={title} /> : <Navigate to="/login" />} />
+            <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+            <Route path="/signup" element={!user ? <Signup /> : <Navigate to="/" />} />
             <Route
               path="/movies/:imdbId"
               element={<MovieForm moviesMDB={movies} />}
