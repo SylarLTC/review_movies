37f153fa157a6fca04c0ecaf045bf0f15e9ec729
Adilzhan <abalgabekov@gmail.com>
2023-05-30 12:59:00 +0100

    Making a useLogin Hook

---
+++ frontend/src/hooks/useLogin.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogin = () => {
+  const [error, setError] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async (email: string, password: string) => {
+    setIsLoading(true);
+    setError(false);
+
+    const response = await fetch("/api/user/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const json = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+      // save the user to local storage
+      localStorage.setItem("user", JSON.stringify(json));
+
+      // update the auth context
+      dispatch({ type: "LOGIN", payload: json });
+
+      setIsLoading(false);
+    }
+  };
+
+  return { login, isLoading, error };
+};
