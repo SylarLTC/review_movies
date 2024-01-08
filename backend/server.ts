0a1150084a34ee7933a2a93e5ac6a928f8ee50c9
Adilzhan <abalgabekov@gmail.com>
2024-01-08 12:22:28 +0000

    heroku

--- backend/server.ts
+++ backend/server.ts
@@ -1,8 +1,9 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 const movieRoute = require("./routes/movieRoutes");
 const userRoute = require("./routes/userRoutes");
+const path = require('path')
 
 dotenv.config();
 
@@ -11,6 +12,18 @@ const port: string | undefined = process.env.PORT;
 
 // middleware
 app.use(express.json());
+
+// creating react static files for heroku
+app.get("/", (req: Request, res: Response) => {
+  res.sendFile(
+    path.join(__dirname, "../frontend/public/index.html"),
+    err => {
+      if (err) {
+        res.status(500).send(err)
+      }
+    }
+  )
+})
 
 // routes
 app.use("/api/movies", movieRoute);
